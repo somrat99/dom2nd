3d0d5df1ed650eae45b4e97fa46c29d262b58367
somrat99 <shahariarsomrat5@gmail.com>
2022-01-17 16:13:15 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+
+const hours = document.querySelector('#hours');
+const munite = document.querySelector('#munite');
+const secend = document.querySelector('#secend');
+
+
+const appm = document.querySelector('#appm');
+const tarikh = document.querySelector('#date');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
+
+const den = document.getElementById('den');
+
+
+
+
+setInterval( function(){
+
+    let date = new Date();
+
+    let h =date.getHours();
+    let m =date.getMinutes();
+    let s =date.getSeconds();
+
+    let dd = date.getDate();
+    let mm = date.getMonth() +1;
+    let yer = date.getFullYear();
+
+    let din= date.getDay();
+
+    den.innerHTML= week(din);
+
+    let hour='';
+
+    if( h==0 ){
+        hour= 12;
+    }else if( h > 0 && h <= 12){
+        hour=h;
+    }else{
+        hour=h-12;
+    }
+
+    hours.innerHTML= hour < 10 ? '0' + hour : hour;
+    munite.innerHTML=m < 10 ? '0' + m : m;
+    secend.innerHTML=s < 10 ? '0' + s : s;
+
+    
+
+    tarikh.innerHTML=dd <10 ? '0' + dd : dd;
+    month.innerHTML=mm < 10 ? '0' + mm : mm;
+    year.innerHTML=yer;
+
+    let pmm = '';
+
+    if( h > 12){
+        pmm = 'PM';
+    }else{
+        pmm ='AM';
+    }
+
+    appm.innerHTML=pmm;
+
+}, 1000);
---
+++ function.js
@@ -0,0 +1,19 @@
+
+function week( deen){
+
+    if( deen == 0){
+        return`Sunday`
+    }else if( deen == 1){
+        return`Monday`
+    }else if( deen == 2){
+        return`Tuesday`
+    }else if( deen == 3){
+        return`Wednesday`
+    }else if( deen == 4){
+        return`Thursday`
+    }else if( deen == 5){
+        return`Friday`
+    }else if( deen == 6){
+        return`Saturday`
+    }
+}
